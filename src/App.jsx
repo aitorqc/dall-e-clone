7ff3a49083011c6fb71453a58d4f99fa2190a1b9
aquicor <aquinoacordero@gmail.com>
2023-02-12 21:39:17 +0100

    create post

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 
 import logo from './assets/logo.svg';
+
 import {Home, CreatePost} from './pages';
 
 const App = () => {
