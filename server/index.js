2fce474cde0fd4aec11059aa659d34bc354910c0
aquicor <aquinoacordero@gmail.com>
2023-02-13 19:02:43 +0100

    backend dalle request

--- server/index.js
+++ server/index.js
@@ -2,18 +2,31 @@ import express from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
 
+import connectDB from './mongodb/connect.js';
+import postRoutes from './routes/postRoutes.js';
+import dalleRoutes from './routes/dalleRoutes.js';
+
 dotenv.config();
 
 const app = express();
 app.use(cors());
-app.use(express.json({limit: "50mb"}));
+app.use(express.json({ limit: "50mb" }));
 
-app.get('/', async(req, res)=>{
+app.use('/api/v1/post', postRoutes);
+app.use('/api/v1/dalle', dalleRoutes);
+
+app.get('/', async (req, res) => {
     res.send("Hello World");
 });
 
 const startServer = async () => {
-    app.listen(8080, () => console.log("Server started on localhost:8080"));
+
+    try {
+        connectDB(process.env.MONGODB_URL);
+        app.listen(8080, () => console.log("Server started on localhost:8080"));
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 startServer();
