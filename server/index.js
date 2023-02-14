5a4584474663362ada023b05499b94012c261474
aquicor <aquinoacordero@gmail.com>
2023-02-14 23:34:25 +0100

    backend dalle request post working

--- server/index.js
+++ server/index.js
@@ -10,23 +10,24 @@ dotenv.config();
 
 const app = express();
 app.use(cors());
-app.use(express.json({ limit: "50mb" }));
+app.use(express.json({ limit: '50mb' }));
 
 app.use('/api/v1/post', postRoutes);
 app.use('/api/v1/dalle', dalleRoutes);
 
 app.get('/', async (req, res) => {
-    res.send("Hello World");
+    res.status(200).json({
+        message: 'Hello from DALL.E Clone!',
+    });
 });
 
 const startServer = async () => {
-
     try {
         connectDB(process.env.MONGODB_URL);
-        app.listen(8080, () => console.log("Server started on localhost:8080"));
+        app.listen(8080, () => console.log('Server started on port 8080'));
     } catch (error) {
         console.log(error);
     }
-}
+};
 
 startServer();
--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -1,6 +1,6 @@
-import express from "express";
+import express from 'express';
 import * as dotenv from 'dotenv';
-import {v2 as cloudinary} from 'cloudinary';
+import { v2 as cloudinary } from 'cloudinary';
 
 import Post from '../mongodb/models/post.js';
 
