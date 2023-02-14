a66f46a2db86d5a22e016aa1068516b61d1951b9
aquicor <aquinoacordero@gmail.com>
2023-02-14 23:34:46 +0100

    backend dalle request post working

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -1,31 +1,44 @@
 import React from 'react'
 
-export default function FormField({ labelName, type, name, placeholder, value, handleChange, isSurpriseMe, handleSurpriseMe }) {
-  return (
-    <div>
-      <div className="flex items-center gap-2 mb-2">
-        <label htmlFor={name} className="block text-sm font-medium text-gray-900">{labelName}</label>
-        {
-          isSurpriseMe && (
-            <button
-              type='button'
-              onClick={handleSurpriseMe}
-              className="font-semibold text-xs bg-[#d2d2e6] py-2 px-3 rounded-[5px] text-black">
-              Surprise Me
-            </button>
-          )
-        }
-      </div>
-      <input
-        type={type}
-        id={name}
-        name={name}
-        placeholder={placeholder}
-        value={value}
-        onChange={handleChange}
-        required
-        className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#4649ff] focus:border-[#4649ff] outline-none block w-full p-3'
-      />
+const FormField = ({
+  labelName,
+  type,
+  name,
+  placeholder,
+  value,
+  handleChange,
+  isSurpriseMe,
+  handleSurpriseMe,
+}) => (
+  <div>
+    <div className="flex items-center gap-2 mb-2">
+      <label
+        htmlFor={name}
+        className="block text-sm font-medium text-gray-900"
+      >
+        {labelName}
+      </label>
+      {isSurpriseMe && (
+        <button
+          type="button"
+          onClick={handleSurpriseMe}
+          className="font-semibold text-xs bg-[#cacae7] py-2 px-3 rounded-[5px] text-black"
+        >
+          Surprise me
+        </button>
+      )}
     </div>
-  )
-}
+    <input
+      type={type}
+      id={name}
+      name={name}
+      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#6469ff] focus:border-[#6469ff] outline-none block w-full p-3"
+      placeholder={placeholder}
+      value={value}
+      onChange={handleChange}
+      required
+    />
+  </div>
+);
+
+export default FormField;
